b02a662430cbbc917b81b981588c674a6614c809
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-29 00:41:19 +0800

    Create HeroSection component.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "./_components/heading";
+import HeroSection from "./_components/hero-section";
 
 export default function MarketingPage() {
   return (
     <div className="flex flex-col min-h-full">
       <div className="flex flex-1 flex-col items-center justify-center md:justify-start text-center gap-y-8 px-6 pb-10">
         <Heading />
+        <HeroSection />
       </div>
     </div>
   );
