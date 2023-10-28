cfedb7cc6a92be71ae066d1ac9aa366c0f658347
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-29 00:25:04 +0800

    Create Marketing Page.

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "./_components/heading";
+
+export default function MarketingPage() {
+  return (
+    <div className="flex flex-col min-h-full">
+      <div className="flex flex-1 flex-col items-center justify-center md:justify-start text-center gap-y-8 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  );
+}
