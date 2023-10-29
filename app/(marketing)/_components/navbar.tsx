a8d8560f0f1e01baad6106392c0212aacf947bfd
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-29 11:54:05 +0800

    Add login button.

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,10 +2,15 @@
 
 import Logo from "@/components/logo";
 import ThemeToggle from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import Spinner from "@/components/ui/spinner";
 import useScrollTop from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 
 export default function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -16,7 +21,19 @@ export default function Navbar() {
     >
       <Logo />
       <div className="flex items-center justify-between gap-x-2 w-full md:ml-auto md:justify-end">
-        Login
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
         <ThemeToggle />
       </div>
     </div>
