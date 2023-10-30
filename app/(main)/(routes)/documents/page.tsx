6918b312e5331a85719cd67912fb4a42623c7b4a
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-30 22:51:10 +0800

    Update Documents Page.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircleIcon } from "lucide-react";
+import Image from "next/image";
+
 export default function DocumentsPage() {
-  return <div>This is a protected page</div>;
+  const { user } = useUser();
+  return (
+    <div className="flex flex-col items-center justify-center h-full space-y-4">
+      <Image
+        src="/empty.png"
+        width="300"
+        height="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        width="300"
+        height="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircleIcon className="mr-2 w-4 h-4" />
+        Create a note
+      </Button>
+    </div>
+  );
 }
