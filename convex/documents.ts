1864016defddad529cdf7c4e4031e936f16bf635
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-06 21:05:01 +0800

    Create getArchived enpoint.

--- convex/documents.ts
+++ convex/documents.ts
@@ -101,3 +101,24 @@ export const archive = mutation({
     return document;
   },
 });
+
+export const getArchived = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArchived"), true))
+      .order("desc")
+      .collect();
+
+    return documents;
+  },
+});
