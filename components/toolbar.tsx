d2e9c4e22cd19a34d59da5512cd81668121efed2
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-10 16:59:38 +0800

    Continue building Toolbar component.

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { ImageIcon, SmileIcon, XIcon } from "lucide-react";
+import { useRef, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
 import IconPicker from "./icon-picker";
 import { Button } from "./ui/button";
-import { SmileIcon, XIcon } from "lucide-react";
 
 export default function Toolbar({
   initialData,
@@ -12,6 +16,38 @@ export default function Toolbar({
   initialData: Doc<"documents">;
   preview?: boolean;
 }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(initialData?.title || "Untitled");
+  const inputRef = useRef<React.ElementRef<"textarea">>(null);
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setTitle(initialData?.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setTitle(value);
+    update({
+      id: initialData?._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
@@ -47,7 +83,35 @@ export default function Toolbar({
             </Button>
           </IconPicker>
         )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            variant="outline"
+            size="sm"
+            className="text-muted-foreground text-xs"
+          >
+            <ImageIcon className="w-4 h-4 mr-2" />
+            Add cover
+          </Button>
+        )}
       </div>
+      {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          value={title}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          onChange={(e) => onInput(e.target.value)}
+          className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+        >
+          {initialData?.title}
+        </div>
+      )}
     </div>
   );
 }
