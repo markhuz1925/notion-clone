be3c32a91075a36e69a3963adc0bfd426652f4ab
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-10 21:53:24 +0800

    Build Document page.

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Toolbar from "@/components/toolbar";
+import Spinner from "@/components/ui/spinner";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+export default function DocumentPage({
+  params,
+}: {
+  params: { documentId: Id<"documents"> };
+}) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found!</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+}
