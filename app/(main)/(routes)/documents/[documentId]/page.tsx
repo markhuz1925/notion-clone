83020d4372d8b6c521a6f4fbadb3e742cf35e4e5
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-11 08:54:34 +0800

    Build Editor component.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import CoverImage from "@/components/cover-image";
+import Editor from "@/components/editor";
 import Toolbar from "@/components/toolbar";
-import Spinner from "@/components/ui/spinner";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 export default function DocumentPage({
   params,
@@ -15,6 +15,7 @@ export default function DocumentPage({
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
+  const update = useMutation(api.documents.update);
 
   if (document === undefined) {
     return (
@@ -28,11 +29,19 @@ export default function DocumentPage({
     return <div>Not found!</div>;
   }
 
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
+
   return (
     <div className="pb-40">
       <CoverImage url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   );
